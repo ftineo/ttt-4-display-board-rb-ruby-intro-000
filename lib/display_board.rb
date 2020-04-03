282ee16e66c330f27147b3a6b6688ef715252ce2
Fernando Tineo Jr <fernandoa.tineo@gmail.com>
2020-04-03 13:39:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board
-  put   |   |   
+  put   |   |
   put-----------
-  put   |   |   
+  put   |   |
   put-----------
-  put   |   |   
-end 
-
+  put   |   |
+end
